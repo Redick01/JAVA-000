2244e6778f924aaabc2f5bf056a6cee397a97d11
penghuiliu <11090829@qq.com>
2020-11-09 23:16:09 +0800

    第四周作业提交

--- Week_04/README.md
+++ Week_04/README.md
@@ -1,1 +1,111 @@
-学习笔记
+
+## 第七课
+
+### 作业
+
+#### 作业1，思考有多少种方式，在main函数启动一个新线程或线程池，异步运行一个方法，拿到这个方法的返回值后，退出主线程
+
+```
+com.thread.homework.Homework03.java
+```
+
+#### 作业2，JAVA多线程与并发脑图
+```
+Java多线程与并发.png
+```
+
+### 学习笔记
+
+#### 到底是有锁还是无锁好
+
+1、并发不高，两者差不多
+2、并发高一点但是不是特别高，无锁好
+3、并发特别高，有锁好
+
+并发特别高的时候，无锁的自旋线程特别多，还不如加锁了
+
+#### 可重入锁
+
+当前线程拿到锁后，后续再次执行获取锁的时候不会重新上锁，而是会使用之前拿到的锁
+
+#### 公平锁
+
+公平锁对每个线程都是公平的，最先排队的线程会拿到锁
+
+#### 非公平锁
+
+非公平锁会随机的将锁给到排队的线程
+
+#### 偏向锁
+
+偏向于之前拿到锁又释放锁的线程
+
+### 第八课
+
+#### JDK基础数据类型与集合及并发问题
+```
+List：
+ArrayList：基于数组，便于按index访问，超过数组需要扩容，扩容成本较高
+安全问题：1、写冲突；2、读写冲突，特别是iterator时，整个数据变了，拿到的数据和实际数据不同，或者产生ConcurrentModificationException
+
+LinkedList：使用链表实现，无需扩容
+安全问题：1、写冲突:两个写，相互操作冲突；2、读写冲突:读，特别是iterator的时候，数据个数变了 ，拿到了非预期数据或者报错产生ConcurrentModificationException
+
+
+Vector、Stack
+Set：LinkedSet、HashSet、TreeSet
+Queue->Deque->LinkedList
+
+Map：HashMap、LinkedHashMap、TreeMap
+Dictionary->HashTable->Propertiess
+```
+#### List线程安全的简单办法
+```
+1、ArrayList所有操作方法上都加synchronized
+2、Collections.synchronizedList，强制将List的操作加上同步
+3、Arrays.asList，不允许添加删除，但是可以set替换元素
+4、Clooection.unmodifiableList，不允许修改内容，包活增加、删除、set
+```
+##### CopyOnWriteArrayList
+```
+核心改进原理：
+1、写加锁，保证不会写乱
+2、写在一个Copy副本上，而不是原始数据上
+
+思考：
+1、插入元素时，在新副本上操作，不影响旧引用
+2、读取不需要加锁？
+读取的时候是在旧应用上，各个线程拿到的数据相同不会出现不一致情况，并且数据使用volatile，保证新副本替换旧引用时候对各个线程立即可见
+```
+#### Map
+
+##### HashMap
+````
+1、基本特点:空间换时间，哈希冲突不大的情况下查找数据性能很高
+2、用途:存放指定key的对象，缓存对象 原理:使用hash原理，存k-v数据，
+初始容量16，扩容x2，负载因子0.75 JDK8以后，在链表长度到8 & 数组长
+度到64时，使用红黑树。
+
+安全问题：
+1、写冲突；
+2、读写问题，可能造成死循环
+多线程进行插入、修改数据时候链表结构可能形成一个环，造成死循环
+
+3、keys()无序问题
+````
+##### LinkedHashMap
+```
+1、基本特点:继承自HashMap，对Entry集合添加了一个双向链表
+2、用途:保证有序，特别是Java8 stream操作的toMap时使用 
+3、原理:同LinkedList，包括插入顺序和访问顺序
+```
+##### ConcurrentHashMap
+```
+Java7 实现：
+    java7中ConcurrentHashMap使用分段锁思想进行实现，在HashMap原有机构上加一层Segment，先进行hash确定Segment，然后在
+确定的Segment的HashMap结构中的操作中加锁，这样其他的Segment不加锁，类似于分库分表思想
+
+Java8实现：
+    java8的实现引入了红黑树，CAS等，具体流程要分析源码
+```
+
---
+++ Week_04/threadtest/src/main/java/com/thread/homework/Homework03.java
@@ -0,0 +1,198 @@
+package com.thread.homework;
+
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Homework03 {
+    private static ExecutorService executorService;
+
+    private final static AtomicInteger threadNumber = new AtomicInteger(1);
+
+    private static final int core = Runtime.getRuntime().availableProcessors() * 2;
+    private static int result = 0;
+
+    private static volatile AtomicInteger semaphore = new AtomicInteger(0);
+
+    public static void main(String[] args) {
+        try {
+            long start=System.currentTimeMillis();
+            // 在这里创建一个线程或线程池，
+            executorService = new ThreadPoolExecutor(core, core,
+                    1000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(1024),
+                    new ThreadFactory() {
+                        @Override
+                        public Thread newThread(Runnable r) {
+                            Thread t = new Thread(Thread.currentThread().getThreadGroup(), r, "homeWork03" + "-thread-" + threadNumber.getAndIncrement(), 0);
+                            return t;
+                        }
+                    }, new ThreadPoolExecutor.CallerRunsPolicy());
+
+            // 异步执行 下面方法
+
+            // 方法1 使用CountDownLatch
+            /*CountDownLatch countDownLatch = new CountDownLatch(1);
+            executorService.execute(() -> {
+                result = sum(); //这是得到的返回值
+                countDownLatch.countDown();
+            });
+            countDownLatch.await();*/
+
+
+            // 方法2 使用 CyclicBarrier 设置两个屏障，如果子线程未到达屏障主线程到达屏障，主线程等待子线程，因为这里是一个线程，所以得这么干，要至少两个线程相互等
+            /*CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
+
+            executorService.execute(() -> {
+                try {
+                    result = sum(); //这是得到的返回值
+                    cyclicBarrier.await();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            cyclicBarrier.await();*/
+
+            // 方法3 使用 CompletableFuture
+            /*result = CompletableFuture.supplyAsync(()->{
+                return sum();
+            }).join();*/
+
+            // 方法4 使用Future
+            /*Future<Integer> result1 = executorService.submit(new Callable<Integer>() {
+                public Integer call() throws Exception {
+                    return sum();
+                }
+            });
+            result = result1.get();*/
+
+            // 方法5，使用FutureTask
+            /*FutureTask<Integer> futureTask = new FutureTask<Integer>(new Callable<Integer>() {
+                @Override
+                public Integer call() throws Exception {
+                    return sum();
+                }
+            });
+            executorService.submit(futureTask);
+            result = futureTask.get();*/
+
+            // 方法6，使用Semaphore，1、在主线程中获取信号量；2、在任务线程中如果处理完释放信号量；3、在提交任务后再次获取信号量，此时只有任务线程释放了信号量主线程才能获取信号量
+            /*final Semaphore semaphore = new Semaphore(1);
+            semaphore.acquire();
+            executorService.execute(() -> {
+                result = sum();
+                semaphore.release();
+            });
+            semaphore.acquire();
+            semaphore.release();*/
+
+            // 方法7 使用阻塞队列LinkedBlockingQueue，1、任务线程将sum()结果添加到队列中；2、主线程take队列，当队列为空时候take阻塞；3、take不为空，不阻塞放行
+            /*LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue();
+            executorService.execute(() -> {
+                result = sum();
+                queue.add(result);
+            });
+            queue.take();*/
+
+            // 方法8 使用Map，或者其他的集合类也可以，该种方式相当于我们自己手动的阻塞主线程，子线程操作与主线程共享内存以达到退出阻塞主线程条件
+            /*Map<Integer, Integer> map = new ConcurrentHashMap<>();
+            executorService.execute(() -> {
+                result = sum();
+                map.put(result, result);
+            });
+            while (map.size() == 0) {
+
+            }*/
+
+            // 方法9 自定义计数器，1、自己定义volatile修饰的计数器；2、当任务线程执行完+1；3、主线程由while阻塞；4、达到条件跳出while
+            /*executorService.execute(() -> {
+                result = sum();
+                semaphore.addAndGet(1);
+            });
+            while (semaphore.intValue() == 0) {
+
+            }*/
+
+            // 方法10 使用wait和notifyAll
+            /*Homework03 homework03 = new Homework03();
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    synchronized (homework03) {
+                        System.out.println("获取到homework03锁，开始执行");
+                    }
+                }
+            });
+            thread.start();
+            executorService.execute(() -> {
+                synchronized (homework03) {
+                    result = sum();
+                    try {
+                        homework03.notifyAll();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            synchronized (homework03) {
+                System.out.println("主线程获取锁");
+                homework03.wait();
+            }*/
+
+
+            // 方法11 使用Condition
+
+            final Lock lock = new ReentrantLock();
+            Condition condition = lock.newCondition();
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        lock.lock();
+                        System.out.println("获取到锁，开始执行");
+                        lock.unlock();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+            });
+            thread.start();
+            executorService.execute(() -> {
+                lock.lock();
+                result = sum();
+                condition.signalAll();
+                lock.unlock();
+            });
+            lock.lock();
+            condition.await();
+            lock.unlock();
+
+
+
+            // 确保  拿到result 并输出
+            System.out.println("异步计算结果为："+result);
+
+            System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+            // 然后退出main线程
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 退出主线程
+            executorService.shutdown();
+        }
+    }
+
+    private static int sum() {
+        return fibo(36);
+    }
+
+    private static int fibo(int a) {
+        if ( a < 2)
+            return 1;
+        return fibo(a-1) + fibo(a-2);
+    }
+}
