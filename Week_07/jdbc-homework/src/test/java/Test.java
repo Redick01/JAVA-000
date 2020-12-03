69c0022dba1887c99a671643807bfd135ccc6876
Redick01 <11090829@qq.com>
2020-12-03 22:42:24 +0800

    111

---
+++ Week_07/jdbc-homework/src/test/java/Test.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author liupenghui
+ * @Date 2020/12/3 4:51 下午
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.get(0);
+    }
+}
